5ebf8b17d986ff201ba4e1a58352173000d90cc2
Andrey Zhdanov <andrey.zhdanov@alumni.aalto.fi>
2023-01-24 01:34:25 +0200

    wip

--- scratch.py
+++ scratch.py
@@ -1,16 +1,19 @@
+import numpy as np
+import pyvista as pv
 import matplotlib.pylab as plt
-from footscan import Step
 
-FNAME = '/home/andrey/storage/Data/Gait_Lab/footscan_export_test/test_test_-_Session_3_-_12-1-2023_-_CadCam_L1.apd'
+from footscan import Session
 
-sc = Step(FNAME)
+FNAME = '/home/andrey/storage/Data/Gait_Lab/footscan_export_test/test_test_-_Session_6_-_12-1-2023_-_CadCam_'
 
+s = Session(FNAME)
 
-plt.figure()
-plt.imshow(sc.dtis[:,:,700])
+step = s.steps[0]
+ny, nx = step.data.shape[:2]
+x = np.arange(nx) * step.dx
+y = np.arange(ny) * step.dy
 
+x, y = np.meshgrid(x, y)
 
-
-
-
-plt.show()
+grid = pv.StructuredGrid(x, y, step.data.max(axis=2))
+grid.plot()
